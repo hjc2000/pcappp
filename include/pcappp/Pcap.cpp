3bb6b11787f0258be75c5a939cb92b5d1ca8fd04
hjc2000 <hjc.666@foxmail.com>
2024-10-22 17:26:35 +0800

    FindInterfaces

--- include/pcappp/Pcap.cpp
+++ include/pcappp/Pcap.cpp
@@ -12,14 +12,11 @@ std::string pcappp::Pcap::Version()
 std::shared_ptr<base::IEnumerable<std::shared_ptr<pcappp::IPcapInterface>>> pcappp::Pcap::FindInterfaces()
 {
     pcappp::PcapInitializer::Initialize();
-
-    std::shared_ptr<base::List<std::shared_ptr<pcappp::IPcapInterface>>> interface_list{
-        new base::List<std::shared_ptr<pcappp::IPcapInterface>>{},
-    };
+    std::shared_ptr<base::List<std::shared_ptr<pcappp::IPcapInterface>>> interface_list{new base::List<std::shared_ptr<pcappp::IPcapInterface>>{}};
 
     // 获取网络接口裸指针集合
-    auto raw_interfaces = pcappp::PcapInterfaceCollection::FindInterfaces();
-    for (auto &raw_interface : *raw_interfaces)
+    std::shared_ptr<pcappp::PcapInterfaceCollection const> raw_interfaces = pcappp::PcapInterfaceCollection::FindInterfaces();
+    for (pcap_if_t *const &raw_interface : *raw_interfaces)
     {
         std::shared_ptr<pcappp::PcapInterface> interface_{new pcappp::PcapInterface{raw_interface}};
         interface_list->Add(interface_);
