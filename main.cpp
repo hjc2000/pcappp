1b4b893d1e2ed926e6ad56fd4c85b3bb20bd2513
hjc2000 <96368006+hjc2000@users.noreply.github.com>
2024-11-26 22:58:24 +0800

    捕获包并且打印出目的MAC，源MAC

--- main.cpp
+++ main.cpp
@@ -1,3 +1,5 @@
+#include <base/net/ethernet/ReadOnlyEthernetFrame.h>
+#include <base/string/ToHexString.h>
 #include <format>
 #include <iostream>
 #include <pcappp/Pcap.h>
@@ -7,7 +9,8 @@ int main()
     std::cout << pcappp::Pcap::Version() << std::endl;
     std::shared_ptr<base::IEnumerable<std::shared_ptr<pcappp::IPcapInterface>>> interface_list = pcappp::Pcap::FindInterfaces();
     // std::shared_ptr<pcappp::IPcapInterface> interface_ = pcappp::Pcap::FindInterfaceByDescription("Network adapter 'Realtek PCIe GbE Family Controller' on local host");
-    std::shared_ptr<pcappp::IPcapInterface> interface_ = pcappp::Pcap::FindInterfaceByDescription("Network adapter 'Intel(R) Wi-Fi 6 AX200 160MHz' on local host");
+    // std::shared_ptr<pcappp::IPcapInterface> interface_ = pcappp::Pcap::FindInterfaceByDescription("Network adapter 'Intel(R) Wi-Fi 6 AX200 160MHz' on local host");
+    std::shared_ptr<pcappp::IPcapInterface> interface_ = pcappp::Pcap::FindInterfaceByDescription("Network adapter 'Intel(R) Ethernet Controller (3) I225-V' on local host");
     if (interface_ != nullptr)
     {
         interface_->Open();
@@ -20,6 +23,11 @@ int main()
         std::cout << interface_->LinkType() << std::endl;
         std::cout << interface_->CaptureResult().Timestamp() << std::endl;
         std::cout << interface_->CaptureResult().CaptureLength() << std::endl;
+
+        base::ethernet::ReadOnlyEthernetFrame frame{interface_->CaptureResult().Buffer()};
+        std::cout << frame.DestinationMac() << std::endl;
+        std::cout << frame.SourceMac() << std::endl;
+        std::cout << frame.TypeOrLength() << std::endl;
     }
 
     return 0;
