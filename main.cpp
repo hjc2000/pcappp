6931e0c29a2559b814fdfeadc040354e44a17e2b
hjc2000 <hjc.666@foxmail.com>
2024-12-31 16:53:08 +0800

    pcap_interface

--- main.cpp
+++ main.cpp
@@ -8,23 +8,23 @@ int main()
 {
     std::cout << pcappp::Pcap::Version() << std::endl;
     std::shared_ptr<base::IEnumerable<std::shared_ptr<pcappp::IPcapInterface>>> interface_list = pcappp::Pcap::FindInterfaces();
-    // std::shared_ptr<pcappp::IPcapInterface> interface_ = pcappp::Pcap::FindInterfaceByDescription("Network adapter 'Realtek PCIe GbE Family Controller' on local host");
-    // std::shared_ptr<pcappp::IPcapInterface> interface_ = pcappp::Pcap::FindInterfaceByDescription("Network adapter 'Intel(R) Wi-Fi 6 AX200 160MHz' on local host");
-    std::shared_ptr<pcappp::IPcapInterface> interface_ = pcappp::Pcap::FindInterfaceByDescription("Network adapter 'Intel(R) Ethernet Controller (3) I225-V' on local host");
-    if (interface_ != nullptr)
+    // std::shared_ptr<pcappp::IPcapInterface> pcap_interface = pcappp::Pcap::FindInterfaceByDescription("Network adapter 'Realtek PCIe GbE Family Controller' on local host");
+    // std::shared_ptr<pcappp::IPcapInterface> pcap_interface = pcappp::Pcap::FindInterfaceByDescription("Network adapter 'Intel(R) Wi-Fi 6 AX200 160MHz' on local host");
+    std::shared_ptr<pcappp::IPcapInterface> pcap_interface = pcappp::Pcap::FindInterfaceByDescription("Network adapter 'Intel(R) Ethernet Controller (3) I225-V' on local host");
+    if (pcap_interface != nullptr)
     {
-        interface_->Open();
-        interface_->CaptureOnePacket();
-        if (interface_->CaptureResult().Code() == pcappp::CaptureResultCode::Success)
+        pcap_interface->Open();
+        pcap_interface->CaptureOnePacket();
+        if (pcap_interface->CaptureResult().Code() == pcappp::CaptureResultCode::Success)
         {
             std::cout << "success" << std::endl;
         }
 
-        std::cout << interface_->LinkType() << std::endl;
-        std::cout << interface_->CaptureResult().Timestamp() << std::endl;
-        std::cout << interface_->CaptureResult().CaptureLength() << std::endl;
+        std::cout << pcap_interface->LinkType() << std::endl;
+        std::cout << pcap_interface->CaptureResult().Timestamp() << std::endl;
+        std::cout << pcap_interface->CaptureResult().CaptureLength() << std::endl;
 
-        base::ethernet::EthernetFrameReader frame{interface_->CaptureResult().Buffer()};
+        base::ethernet::EthernetFrameReader frame{pcap_interface->CaptureResult().Buffer()};
         std::cout << frame.DestinationMac() << std::endl;
         std::cout << frame.SourceMac() << std::endl;
         std::cout << base::ToString(frame.TypeOrLength()) << std::endl;
